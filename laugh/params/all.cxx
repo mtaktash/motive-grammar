7eaddd035981ca668ae8d8cd27ec19e3abcd050d
mtaktash <mariataktasheva@gmail.com>
2017-10-11 14:10:56 +0300

    Laughing process grammar

--- laugh/params/all.cxx
+++ laugh/params/all.cxx
@@ -2,35 +2,42 @@
 #GRAMMAR_ROOT S      // указываем корневой нетерминал грамматики
 
 ProperName ->  Word<h-reg1>+;                  
-Person -> ProperName | 'человек'; 
-FormOfAddress -> 'товарищ' | 'мистер' | 'господин';   // перечисление возможных форм обращения
+Who -> ProperName | Noun; 	
+
+LaughVerbs -> Verb<kwtype="глагол-смеха">;	
+LaughNouns -> Noun<kwtype="существительное-смеха">;	
+
+LaughWithVerb -> Adv* Word* LaughVerbs Word* Adv*;
+LaughWithNoun -> Verb Adj* LaughNouns;
+
+Laugh -> LaughWithVerb | LaughWithNoun;
+
+S -> Word* Comma* Who Laugh Comma* Word*;
 
 
-LaughNouns -> 'смех' | 'хохот' | 'гогот' | 'смешок' 
-					 | 'шутка' | 'веселье' | 'хихиканье';	
-
-LaughVerbs -> 'смеяться' | 'усмехнуться' | 'хохотать' |
-			'хихикать' | 'гоготать' | 'ржать' | 
-			'грохотать' | 'кудахтать' | 'прыснуть';				
-
-// существительные
-S ->  Adj+ LaughNouns;  
-S ->  Verb LaughNouns;  
-S ->  Verb Adj+ LaughNouns;
-
-S ->  Person Verb LaughNouns;    
-S ->  Person Verb Adj+ LaughNouns;    
 
 
-// глаголы
-S ->  Person LaughVerbs;  
-S ->  Noun LaughVerbs;  
-// над чем
-S ->  Person LaughVerbs Prep Noun;  
-S ->  Noun LaughVerbs Prep Noun;  
 
-// как  
-S ->  Person Adv LaughVerbs;  
-S ->  Noun Adv LaughVerbs;   
+
+
+
+// // существительные
+// S ->  Adj+ LaughNouns;  
+// S ->  Verb LaughNouns;  
+// S ->  Verb Adj+ LaughNouns;
+
+// S ->  Person Verb LaughNouns;    
+// S ->  Person Verb Adj+ LaughNouns;    
+
+
+// // глаголы
+// S ->  Person LaughVerbs;  
+// S ->  Noun LaughVerbs;  
+// // над чем
+// S ->  Person LaughVerbs Prep Noun;  
+
+// // как  
+// S ->  Person Adv LaughVerbs;  
+// S ->  Noun Adv LaughVerbs;   
 
 
